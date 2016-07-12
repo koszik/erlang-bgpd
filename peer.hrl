e2a3206cd82b11454117f13a591c41995dcfb87c
koszik <koszik@atw.hu>
2016-07-12 04:43:33 +0200

    hrl

---
+++ peer.hrl
@@ -0,0 +1,2 @@
+-define(ATTRIB_RECORD, optional, transitive, partial, type, data, type_decoded, data_decoded, origin, as_path, next_hop, med, local_pref, atomic_aggregate, aggregator, transitive_unknown).
+-record(attrib, {?ATTRIB_RECORD}).
