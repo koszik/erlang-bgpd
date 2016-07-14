3e7316dca9176a4a2ca753592d8e3c7a4b873251
koszik <koszik@atw.hu>
2016-07-14 03:10:33 +0200

    misc cleanups, attrib handling via the new way only

--- peer.hrl
+++ peer.hrl
@@ -1,2 +1,1 @@
--define(ATTRIB_RECORD, optional, transitive, partial, type, data, type_decoded, data_decoded, origin, as_path, next_hop, med, local_pref, atomic_aggregate, aggregator, transitive_unknown).
--record(attrib, {?ATTRIB_RECORD}).
+-record(attrib, {unknown, origin, as_path, next_hop, med, local_pref, atomic_aggregate, aggregator, aggregator_partial}).
