b837eb8dac5a845dfc8ddad759e506a2467f0e09
koszik <koszik@atw.hu>
2016-07-31 20:36:16 +0200

    fix unknown attribs list

--- peer.hrl
+++ peer.hrl
@@ -1,1 +1,1 @@
--record(attrib, {unknown, origin, as_path, next_hop, med, local_pref, atomic_aggregate, aggregator, aggregator_partial}).
+-record(attrib, {unknown = [], origin, as_path, next_hop, med, local_pref, atomic_aggregate, aggregator, aggregator_partial}).
