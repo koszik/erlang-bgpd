b2c771a4eed7a82263fc712f3a2f5da47f18c704
koszik <koszik@atw.hu>
2016-11-24 03:41:10 +0100

    abstract process store

--- process_manager.erl
+++ process_manager.erl
@@ -2,6 +2,7 @@
 -behaviour(gen_server).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, code_change/3, terminate/2]).
 -export([start/0, register/1, register/2, cast/2, show/1]).
+-export([get_key/1, get_pid/1]).
 
 handle_call({register, ID, Pid}, _From, State) ->
     case ets:insert_new(process_store, {ID, Pid}) of
@@ -49,10 +50,23 @@ register(ID) ->
 register(ID, Pid) ->
     gen_server:call(?MODULE, {register, ID, Pid}).
 
+
+get_pid(Key) ->
+    ets:match(process_store, {Key, '$1'}).
+
+get_key(Pid) ->
+    ets:match(process_store, {'$1', Pid}).
+
 cast(Selector, Msg) ->
-    case ets:match(process_store, {Selector, '$1'}) of
+    case get_pid(Selector) of
 	[] -> notfound;
-	[[Pid]] -> Pid ! Msg, ok
+	[[Pid]] -> gen_server:cast(Pid, Msg)
+    end.
+
+call(Selector, Msg) ->
+    case get_pid(Selector) of
+	[] -> exit(notfound);
+	[[Pid]] -> gen_server:call(Pid, Msg)
     end.
 
 start() ->
